e9954dd1cad3ad6ee7fdfa3b0987717307ba6d1a
Ingo Berg <github_contact@beltoforion.de>
2020-06-03 00:14:17 +0200

    Update example1.cpp
    better compiler identification

--- samples/example1/example1.cpp
+++ samples/example1/example1.cpp
@@ -191,12 +191,18 @@ static void Splash()
 	mu::console() << _T("  Version ") << Parser().GetVersion(pviFULL) << _T("\n");
 	mu::console() << _T("  (C) 2004 - 2020 Ingo Berg\n");
 	mu::console() << _T("-----------------------------------------------------------\n");
-#if defined (__GNUC__)
-	mu::console() <<  _T("  Compiled with GCC Version ") << __GNUC__ << _T(".") << __GNUC_MINOR__ << _T(".") << __GNUC_PATCHLEVEL__ << _T("\n");
-#elif defined(_MSC_VER)
-	mu::console() << _T("  Compiled with MSVC Version ") << _MSC_VER <<  _T("\n");
-#elif defined(__clang__)
+
+#if defined(__clang__)
+	// Note: CLANG also identifies as GCC 4.2.1
 	mu::console() << _T("  Compiled with CLANG Version ") << __clang_major__ << _T(".") << __clang_minor__ << _T(".") << __clang_patchlevel__ << _T("\n");
+#else
+
+#if defined (__GNUC__)
+	mu::console() << _T("  Compiled with GCC Version ") << __GNUC__ << _T(".") << __GNUC_MINOR__ << _T(".") << __GNUC_PATCHLEVEL__ << _T("\n");
+#elif defined(_MSC_VER)
+	mu::console() << _T("  Compiled with MSVC Version ") << _MSC_VER << _T("\n");
+#endif
+
 #endif
 
 	mu::console() << _T("  IEEE 754 (IEC 559) is ") << ((std::numeric_limits<double>::is_iec559) ? "Available" : " NOT AVAILABLE") << _T("\n");
