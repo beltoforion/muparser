a91a9f2ec905469ed56f3c26bed3c75302774abc
Ingo Berg <github_contact@beltoforion.de>
2020-06-16 00:06:46 +0200

    Update muParserDef.h
    removed "prerelease" from version number

--- include/muParserDef.h
+++ include/muParserDef.h
@@ -416,8 +416,8 @@ namespace mu
 
 	static const int MaxLenExpression = 5000;
 	static const int MaxLenIdentifier = 100;
-	static const string_type ParserVersion = string_type(_T("2.3.1 (Prerelease)"));
-	static const string_type ParserVersionDate = string_type(_T("20200613"));
+	static const string_type ParserVersion = string_type(_T("2.3.1"));
+	static const string_type ParserVersionDate = string_type(_T("20200616"));
 } // end of namespace
 
 #if defined(_MSC_VER)
